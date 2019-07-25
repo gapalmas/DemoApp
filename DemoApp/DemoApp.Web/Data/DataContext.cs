d1882cc73467744368a6a4361bf6f525ce314a60
Guillermo Palma <gapalmasolano@gmail.com>
2019-07-25 13:32:18 -0700

    Base de datos

---
+++ DemoApp/DemoApp.Web/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoApp.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
--- DemoApp/DemoApp.Web/Startup.cs
+++ DemoApp/DemoApp.Web/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DemoApp.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +21,12 @@ namespace DemoApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseMySql(this.Configuration.GetConnectionString("MySQLConnection"));
+                //cfg.UseSqlServer(this.Configuration.GetConnectionString("SQLServerConnection"));
+            });
+             
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
