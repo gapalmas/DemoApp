ba66f85321cd5e4e3971b50b85e6fe4aec0447a1
Guillermo Palma <gapalmasolano@gmail.com>
2019-07-31 13:11:48 -0700

    Migracion User

--- DemoApp/DemoApp.Web/Data/DataContext.cs
+++ DemoApp/DemoApp.Web/Data/DataContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DemoApp.Web.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoApp.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
---
+++ DemoApp/DemoApp.Web/Models/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace DemoApp.Web.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
