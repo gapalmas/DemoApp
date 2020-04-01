d7c475fae58b94448f879f8894fe7daac210de0b
Guillermo Palma <gapalmasolano@gmail.com>
2020-04-01 10:02:55 -0700

    Migrate 2.2 to 3.1 net core

--- DemoApp/DemoApp.Web/Startup.cs
+++ DemoApp/DemoApp.Web/Startup.cs
@@ -54,13 +54,14 @@ namespace DemoApp.Web
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc().AddMvcOptions(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName == "Development")
             {
                 app.UseDeveloperExceptionPage();
             }
